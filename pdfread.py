f3eb4a60326083e52245e0d2ce2a9e13c4f266ed
Dhruv K. Aggarwal <165761845+Dhruv-KAggarwal@users.noreply.github.com>
2025-02-27 11:34:39 +0530

    Update pdfread.py

--- pdfread.py
+++ pdfread.py
@@ -71,9 +71,9 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with Judgements 💁")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask a Question from the Files")
 
     if user_question:
         user_input(user_question)
